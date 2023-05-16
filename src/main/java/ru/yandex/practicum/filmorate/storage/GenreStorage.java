8bb1115bb2474fe618fec7c4f47574bafa85c1dd
Mariachern <mariechernyavskaya@gmail.com>
2023-05-16 16:44:01 +0300

    first review

--- src/main/java/ru/yandex/practicum/filmorate/storage/GenreStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/GenreStorage.java
@@ -1,13 +1,9 @@
 package ru.yandex.practicum.filmorate.storage;
 
 import org.springframework.stereotype.Component;
-import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 
-import java.util.Collection;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 @Component
 public interface GenreStorage {
